7239f2a7cbe8812f6cfb92c57f92186d930bf930
NISHANT TAMILSELVAN <nishant.crook@outlook.com>
2016-10-04 01:20:05 +0200

    Take a rest Application

--- Take_a_rest/take_a_rest.py
+++ Take_a_rest/take_a_rest.py
@@ -1,4 +1,8 @@
 # Take a rest Application
 import webbrowser  # To use web browser functionalities.
+import time        # To work with system time.
 
-webbrowser.open_new_tab("https://www.youtube.com/watch?v=F1ZT8XyxyH4")
+while true:
+    time.sleep(60) # will sleep for given sec 60 = 1min
+    #open the default browser and then open a new tab and run the given link
+    webbrowser.open_new_tab("https://www.youtube.com/watch?v=F1ZT8XyxyH4")
